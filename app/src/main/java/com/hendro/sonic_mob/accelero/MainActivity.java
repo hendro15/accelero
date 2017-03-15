8de352ce90250864736053e6a9e87a8430256459
hendro15 <hendro.prabowo15@gmail.com>
2017-03-15 12:46:47 +0700

    add code to detect sensor shake

--- app/src/main/java/com/hendro/sonic_mob/accelero/MainActivity.java
+++ app/src/main/java/com/hendro/sonic_mob/accelero/MainActivity.java
@@ -1,13 +1,75 @@
 package com.hendro.sonic_mob.accelero;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            // check sensor every 100 milisecond
+            long currentTime = System.currentTimeMillis();
+            if ((currentTime - lastUpdate) > 100){
+                long diffTime = (currentTime - lastUpdate);
+                lastUpdate = currentTime;
+
+                // chechk if device has been shaken or not. Modifying this increase or decrease the
+                // sensitivity so feel free to play with its value.
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/diffTime*10000;
+
+                if (speed > SHAKE_THRESHOLD){
+
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
